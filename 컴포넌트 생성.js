277e80f191110a7de2e8e4781144139d15c58d7f
강성훈 <yolly050101@naver.com>
2021-07-11 15:53:17 +0900

    컴포넌트 생성

---
+++ 컴포넌트 생성.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MyComponent from "./MyComponents"; // 모듈 물러오기
+import "./App.css";
+
+const App = () => {
+  return <MyComponent />;
+};
+export default App;
---
+++ 클래스형 컴포넌트.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const name = "리액트";
+    return <div className="react">{name}</div>;
+  }
+}
+export default App;
