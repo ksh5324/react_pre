6c74e5f5ce574cc5e460c4cb89ae7de4cd07bd5a
강성훈 <yolly050101@naver.com>
2021-08-08 18:04:07 +0900

    컴포넌트 반복

---
+++ 컴포넌트 반복/IterationSample2.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const IterationSample2 = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  return (
+    <div>
+      <input value={inputText} onChange={onChange}></input>
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </div>
+  );
+};
+
+export default IterationSample2;
