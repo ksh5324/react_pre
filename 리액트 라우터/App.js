ca9ee084b875a5d25a0cc8d5c37c4b76b26124f1
강성훈 <yolly050101@naver.com>
2021-08-11 15:26:27 +0900

    라우터

---
+++ 리액트 라우터/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route } from "react-router";
+import { Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} exact={true} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
+    </div>
+  );
+};
+export default App;
---
+++ 리액트 라우터/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 입니다.</p>
+    </div>
+  );
+};
+export default Home;
