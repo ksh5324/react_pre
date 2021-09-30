d3a253848bec6668f2dd1d75a5432bd8bc408a82
강성훈 <yolly050101@naver.com>
2021-09-30 18:15:41 +0900

    지뢰찾기1

---
+++ react 기초부터 다시 쌓기/지뢰찾기 /App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MineSearch from "./MineSearch";
+
+const App = () => {
+  return <MineSearch />;
+};
+
+export default App;
---
+++ react 기초부터 다시 쌓기/지뢰찾기 /NumberBaseBall.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import Try from "./Try";
+
+const getNumbers = () => {
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i), 1))[0];
+    array.push(chosen);
+  }
+  return array;
+};
+
+class NumberBaseBall extends Component {
+  state = {
+    result: "",
+    value: "",
+    answer: getNumbers(),
+    tries: [],
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.value === this.state.answer.join("")) {
+      this.setState({
+        result: "홈런",
+        tries: [
+          ...this.state.tries,
+          { try: this.state.value, result: "홈런!" },
+        ],
+      });
+      alert("게임을 다시 시작합니다");
+      this.setState({
+        answer: getNumbers(),
+        value: "",
+        tries: [],
+      });
+    } else {
+      const answerArray = this.state.value.split("").map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (this.state.tries.length >= 9) {
+        this.setState({
+          result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(
+            ""
+          )}였습니다!`,
+        });
+        alert("게임을 다시 시작합니다");
+        this.setState({
+          answer: getNumbers(),
+          value: "",
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i++) {
+          if (answerArray[i] === this.state.answer[i]) {
+            strike += 1;
+          } else if (this.state.answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState({
+          tries: [
+            ...this.state.tries,
+            {
+              try: this.state.value,
+              result: `${strike} 스트라이크, ${ball} 볼 입니다.`,
+            },
+          ],
+        });
+      }
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            maxLength={4}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {this.state.tries.map((v, i) => {
+            return <Try key={`${i + 1} 차 시도 : `} tryInfo={v} />;
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default NumberBaseBall;
