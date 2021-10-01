f4ee484ad27c0b800f554141c43c75e530d9c3ba
강성훈 <yolly050101@naver.com>
2021-10-01 21:51:46 +0900

    지뢰찾기 3

--- react 기초부터 다시 쌓기/지뢰찾기/MineSearch.jsx
+++ react 기초부터 다시 쌓기/지뢰찾기/MineSearch.jsx
@@ -26,9 +26,38 @@ const initialState = {
 
 const plantMine = (row, cell, mine) => {
   console.log(row, cell, mine);
+  const candidate = Array(row * cell)
+    .fill()
+    .map((arr, i) => {
+      return i;
+    });
+  const shuffle = [];
+  while (candidate.length > row * cell - mine) {
+    const chosen = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1
+    )[0];
+    shuffle.push(chosen);
+  }
+  const data = [];
+  for (let i = 0; i < row; i++) {
+    const rowData = [];
+    data.push(rowData);
+    for (let j = 0; j < cell; j++) {
+      rowData.push(CODE.NORMAL);
+    }
+  }
+
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+    data[ver][hor] = CODE.MINE;
+  }
+  return data;
 };
 
 export const START_GAME = "START_GAME";
+export const OPEN_CELL = "OPEN_CELL";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -37,6 +66,15 @@ const reducer = (state, action) => {
         ...state,
         tableData: plantMine(action.row, action.cell, action.mine),
       };
+    case OPEN_CELL: {
+      const tableData = [...state, tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.OPENED;
+      return {
+        ...state,
+        tableData,
+      };
+    }
     default:
       return state;
   }
